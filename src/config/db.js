e929989bb0206a15dc3543b47403ceef4b3c2440
Nvllew <nivllew@gmail.com>
2026-05-31 11:51:09 -0600

    Avance 31 Mayo 2026

--- src/config/db.js
+++ src/config/db.js
@@ -1,17 +1,19 @@
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
-import { env } from "./env.js";
 
 dotenv.config();
 
-const pool = mysql.createPool({
-  host: env.DB_HOST,
-  user: env.DB_USER,
-  password: env.DB_PASSWORD,
-  database: env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
+const pool = process.env.MYSQL_PUBLIC_URL
+  ? mysql.createPool(process.env.MYSQL_PUBLIC_URL)
+  : mysql.createPool({
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      waitForConnections: true,
+      connectionLimit: 10,
+      queueLimit: 0,
+    });
 
 export default pool;
